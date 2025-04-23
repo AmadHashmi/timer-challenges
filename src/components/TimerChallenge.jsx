9db785c7495aee15cfe47c00796b90f5778a1e8c
Amad Ul Hassan <ah@indigilabs.com>
2025-04-23 22:44:27 +0200

    using refs to update the time stop
    using refs to update the time stop

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,37 @@
+import { useRef, useState } from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  const [timerExpired, setTimerExprire] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+  //   let timer;
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExprire(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+    setTimerStarted(false);
+  }
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
+    </section>
+  );
+}
